edcfac683dec798e29cd58cf91358a8dd9e9d622
Tom Ruette <tom.ruette@gmail.com>
2019-12-02 17:38:25 +0100

    some fixtures for entity and relation types

--- catalog/fixtures/csv_to_json.py
+++ catalog/fixtures/csv_to_json.py
@@ -1,27 +1,27 @@
 from json import dumps
 from codecs import open
 
-with open("./catalog/fixtures/entity_and_relation_types.csv", "r", "utf-8") as f:
+with open("./catalog/fixtures/productions.csv", "r", "utf-8") as f:
     lines = f.read().split("\n")
 
-header = lines[0].strip().split(",")
+header = lines[0].strip().split("\t")
 print(header)
 
 fixtures = []
 
 for line in lines[1:]:
     fixture = {"fields": {}}
-    print(line.split(","))
-    for i, value in enumerate(line.strip().split(",")):
+    print(line.split("\t"))
+    for i, value in enumerate(line.strip().split("\t")):
         if len(value) > 0:
             key = header[i]
             print(key, value)
             if key in ["model", "pk"]:
                 fixture[key.replace(" ", "_")] = value
-            if key in ["name", "inverted name"]:
+            if key in ["name", "sort_name", "disambiguation"]:
                 fixture["fields"][key.replace(" ", "_")] = value
 
     fixtures.append(fixture)
 
-with open("./catalog/fixtures/entity_and_relation_types.json", "w", "utf-8") as f:
+with open("./catalog/fixtures/productions.json", "w", "utf-8") as f:
     f.write(dumps(fixtures, indent=4))
